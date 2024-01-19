a819e237eec582e90559449f108e66cd1e9454e7
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2024-01-19 23:49:50 +0100

    Fix missing 'models' imports in Team model

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Team(models.Model):
-    name_abbreviation = CharField(max_length=3, unique=True, blank=False, null=False)
-    full_name = CharField(max_length=100, unique=True, blank=False, null=False)
+    name_abbreviation = models.CharField(max_length=3, unique=True, blank=False, null=False)
+    full_name = models.CharField(max_length=100, unique=True, blank=False, null=False)
 
     def __str__(self):
         return self.name_abbreviation
