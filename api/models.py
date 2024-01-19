47b6e12d91d02bd1809453da2350ae97370eee9f
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2024-01-20 00:00:11 +0100

    Add Coach model
    Create a Coach model consisting of name, date_of_birth and current_team, which can be null. Resolves #3.

--- api/models.py
+++ api/models.py
@@ -7,4 +7,9 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name_abbreviation
-    
+
+
+class Coach(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    date_of_birth = models.DateField(blank=True, null=True)
+    current_team = models.ForeignKey('Team', related_name='coach', on_delete=models.SET_NULL, null=True, blank=True)
