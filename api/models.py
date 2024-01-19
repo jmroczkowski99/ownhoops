bf727fae44ba327dc147c6a11c9e467e0816df58
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2024-01-20 00:06:54 +0100

    Add missing str method to a Coach model

--- api/models.py
+++ api/models.py
@@ -13,3 +13,7 @@ class Coach(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     date_of_birth = models.DateField(blank=True, null=True)
     current_team = models.ForeignKey('Team', related_name='coach', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
